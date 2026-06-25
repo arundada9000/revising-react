46e0eba5c5f9cee0048d083fe45ace71c10aac5d
Arun Neupane <arunneupane0000@gmail.com>
2026-06-25 09:40:00 +0545

    feat: add Github component with user data fetched from API

---
+++ src/components/Github.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+const Github = () => {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch("https://api.github.com/users/arundada9000")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl flex flex-col items-center justify-center gap-2">
+      <h1>Github Followers: {data.followers}</h1>
+      <img src={data.avatar_url} alt="Arun Neupane" />
+    </div>
+  );
+};
+
+export default Github;
