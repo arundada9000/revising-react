7c8495a7fc18ec99dece534ab928239aec7348bb
Arun Neupane <arunneupane0000@gmail.com>
2026-06-25 08:40:00 +0545

    feat: install react-router-dom and set up browser routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,67 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import App from "./App.jsx";
+import Layout from "./components/Layout.jsx";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
+import User from "./components/User.jsx";
+import Github from "./components/Github.jsx";
+
+import ResourceLayout from "./components/ResourceLayout.jsx";
+import Resources from "./components/Resources.jsx";
 import Counter from "./components/Counter.jsx";
-import Card from "./components/Card.jsx";
+// import Card from "./components/Card.jsx";
 import BgChanger from "./components/BgChanger.jsx";
 import PasswordGenerator from "./components/PasswordGenerator.jsx";
 
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       { path: "", element: <Home /> },
+//       { path: "about", element: <About /> },
+//       { path: "contact", element: <Contact /> },
+//       { path: "about", element: <About /> },
+//     ],
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements([
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />}></Route>
+      <Route path="contact" element={<Contact />}></Route>
+      <Route path="about" element={<About />}></Route>
+      <Route path="user/:userid" element={<User />}></Route>
+      <Route path="github" element={<Github />}></Route>
+    </Route>,
+    <Route path="/resources" element={<ResourceLayout />}>
+      <Route path="" element={<Resources />}></Route>
+      <Route path="bgchanger" element={<BgChanger />}></Route>
+      <Route path="passwordgenerator" element={<PasswordGenerator />}></Route>
+      <Route path="counter" element={<Counter />}></Route>
+      <Route path="currencyconverter" element={<App />}></Route>
+    </Route>,
+  ]),
+);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* <App />
     <Counter />
     <Card name="Arun Neupane" btnText="Click Me" />
     <Card name="Thesthesti" btnText="Visit Me" />
     <BgChanger />
-    <PasswordGenerator />
+    <PasswordGenerator /> */}
+    <RouterProvider router={router} />
   </StrictMode>,
 );
