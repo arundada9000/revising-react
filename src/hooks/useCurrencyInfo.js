7cb06722387fbfbf3a9da52a0203a6c9fe0ceffb
Arun Neupane <arunneupane0000@gmail.com>
2026-06-25 07:20:00 +0545

    feat: add useCurrencyInfo custom hook for fetching exchange rates

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  const url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`;
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+  }, [currency, url]);
+
+  return data;
+}
+
+export default useCurrencyInfo;
