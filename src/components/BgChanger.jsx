4eb918669d5c06e6cc306f5a028410aedaa0b835
Arun Neupane <arunneupane0000@gmail.com>
2026-06-25 08:00:00 +0545

    fix: lowercase CSS color value in BgChanger for proper rendering

--- src/components/BgChanger.jsx
+++ src/components/BgChanger.jsx
@@ -28,7 +28,7 @@ const BgChanger = () => {
         Red
       </button>
       <button
-        onClick={() => setCurrentColor("Blue")}
+        onClick={() => setCurrentColor("blue")}
         className="bg-blue-400 text-white cursor-pointer px-4 py-2 rounded-xl"
       >
         Blue
